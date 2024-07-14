6e31f06fde919214fcd221226858f2f364d299bc
italan <italanleal@gmail.com>
2024-07-14 09:39:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function foo(){
+    const CTRL_SHIFT_U_KEY = {
+        key: 'u',
+        code: 'KeyU',
+        keyCode: 85,
+        ctrlKey: true,
+        shiftKey: true
+    }
+   
+    function keyHandler(key){
+        function call(){
+            document.dispatchEvent(new KeyboardEvent('keydown', key)); 
+            setTimeout(() => document.dispatchEvent(new KeyboardEvent('keyup', key)), 100);
+        } return { call };
+    }
+
+    function keep(){
+        const interval = setInterval((() => document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click()), 90000);
+       
+        function clear() {
+            clearInterval(interval);
+        } return { clear };
+    }
+
+    function captcha(){
+        document.querySelector("mwc-dialog.yes-no-dialog").querySelectorAll("mwc-button")[1].shadowRoot.querySelector("button").click();
+    }
+
+    setTimeout(keep().clear, 2880000);
+    keyHandler(CTRL_SHIFT_U_KEY).call();
+    setTimeout(captcha, 8000);
+
+    return setInterval(function() {
+
+        setTimeout(keep().clear, 2880000);
+        keyHandler(CTRL_SHIFT_U_KEY).call();
+        setTimeout(captcha, 8000);
+
+    }, 2888000);
+    
+} foo();  
